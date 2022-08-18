a559a9e443d8afa212ecd040b8c855a13f98cb7f
Alex Cutforth <alex@densem.net>
2022-08-18 13:49:08 +1200

    convert to pros project

--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,52 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       Alex Cutforth                                             */
-/*    Created:      Wednesday 17 August 2022                                  */
-/*    Description:  Program for team 4067X's robot for the spin up season     */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
+#include "main.h"
+#include "okapi/api/chassis/controller/chassisController.hpp"
+#include "okapi/api/chassis/controller/chassisControllerIntegrated.hpp"
+#include "okapi/api/chassis/controller/chassisControllerPid.hpp"
+#include "okapi/api/util/mathUtil.hpp"
+#include "okapi/impl/chassis/controller/chassisControllerBuilder.hpp"
+#include "okapi/impl/device/controller.hpp"
+#include "okapi/impl/device/controllerUtil.hpp"
+#include "pros/llemu.hpp"
+#include "pros/misc.h"
+#include "pros/motors.h"
 
-#include "vex.h"
+using namespace okapi;
+using namespace std;
 
-using namespace vex;
+Controller master;
 
-competition Competition;
-controller Controller;
+shared_ptr<ChassisController> chassis = 
+	ChassisControllerBuilder()
+		.withMotors(
+			{ -1, 11 },
+			{ 10, -19 }
+		).withDimensions(AbstractMotor::gearset::green, {{ 4_in, 11.5_in}, imev5GreenTPR})
+		.withGains(     
+			{0.001, 0, 0.0001}, // Distance controller gains
+			{0.001, 0, 0.0001},     // Turn controller gains
+			{0.001, 0, 0.0001}     // Angle controller gains (helps drive straight)
+		).withOdometry()
+		.buildOdometry();
 
-motor left_drive_front = motor(PORT1, ratio18_1, true);
-motor left_drive_back = motor(PORT11, ratio18_1);
-
-motor right_drive_front = motor(PORT10, ratio18_1);
-motor right_drive_back = motor(PORT19, ratio18_1, true);
-
-motor_group left_drive = motor_group(left_drive_front, left_drive_back);
-motor_group right_drive = motor_group(right_drive_front, right_drive_back);
-
-const float ACCELERATION_LIMIT = 20;
-
-float current_left_speed = 0.0;
-float current_right_speed = 0.0;
-
-int sign(float x) {
-  if(x > 0) {
-    return 1;
-  } else if(x < 0) {
-    return -1;
-  } else {
-    return 0;
-  }
+void initialize() {
+	pros::lcd::initialize();
+	pros::lcd::set_text(1, "4067X Spin Up Program");
 }
 
-void drive_tank(int left_target, int right_target) {
-  current_left_speed += ACCELERATION_LIMIT * sign(left_target - current_left_speed);
-  current_right_speed += ACCELERATION_LIMIT * sign(right_target - current_right_speed);
+void disabled() {}
 
-  left_drive.spin(fwd, current_left_speed, pct);
-  right_drive.spin(fwd, current_right_speed, pct);
-}
- 
-void pre_auton(void) {
-  vexcodeInit();
-}
+void competition_initialize() {}
 
-void autonomous(void) {
-  
-}
+void autonomous() {}
 
-void user_control(void) {
-  while (true) {
-    drive_tank(Controller.Axis3.position(), Controller.Axis2.position());
-    wait(20, msec);
-  }
-}
+void opcontrol() {
+	while (true) {
+		chassis->getModel()->tank(
+			master.getAnalog(ControllerAnalog::leftY), 
+			master.getAnalog(ControllerAnalog::rightY), 
+			0.05
+		);
 
-int main() {
-  Competition.autonomous(autonomous);
-  Competition.drivercontrol(user_control);
-
-  pre_auton();
-
-  while (true) {
-    wait(100, msec);
-  }
+		pros::delay(20);
+	}
 }
