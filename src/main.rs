4c7793785b302c54985f30706f1923406217323f
Alex Cutforth <alex@densem.net>
2022-08-30 22:03:01 +1200

    hello world bot test bench

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+#![no_std]
+#![no_main]
+
+use vex_rt::prelude::*;
+struct HelloBot;
+impl Robot for HelloBot {
+    fn new(_peripherals: Peripherals) -> Self {
+        println!("Hello, world");
+        HelloBot
+    }
+}
+entry!(HelloBot);
